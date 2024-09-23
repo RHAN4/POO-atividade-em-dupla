08acde7d8d8cf908e7688b3b555164eee690f5a0
Rhana <159554135+XurumelaDeCalcinha@users.noreply.github.com>
2024-09-23 13:17:21 +0000

    ediçõesss

--- projeto/models/pessoa.py
+++ projeto/models/pessoa.py
@@ -1,8 +1,15 @@
 class Pessoa:
     def __init__(self, nome: str, idade: int) -> None:
         self.nome = nome
-        self.idade = idade
-        
+        self.idade = self._verificar_idade(idade)
+
+    def _verificar_idade(self, idade):
+        if idade < 0:
+            raise ValueError("Idade não pode ser negativa.")
+        if not isinstance(idade, int):
+            raise TypeError("Idade deve conter apenas números.")
+        return idade
+
     def __str__(self) -> str:
         return (f"\nNome: {self.nome}"
                 f"\nIdade: {self.idade}")
