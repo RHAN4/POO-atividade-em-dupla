64a9811baba5474187f9c27c9ae3c75456290a2c
Rhana <159554135+XurumelaDeCalcinha@users.noreply.github.com>
2024-09-23 11:50:55 +0000

    git ignore

--- projeto/tests/test_pessoa.py
+++ projeto/tests/test_pessoa.py
@@ -6,5 +6,11 @@ def pessoa_valida():
     pessoa = Pessoa("Marta", "23")
     return pessoa
 
+#Alterando o nome da pessoa de "Marta" para "Vitoria"
+def alterando_pessoa_nome(pessoa_valida):
+    pessoa_valida.nome = "Vitoria"
+    assert pessoa_valida.nome == "Vitoria"
+
+
 def test_pessoa_nome_valido(pessoa_valida):
     assert pessoa_valida.nome == "Marta"
